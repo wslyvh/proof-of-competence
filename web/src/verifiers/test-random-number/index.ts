23864c500e0984cc66634239529a28adbd0d3b55
wslyvh <wslyvh@users.noreply.github.com>
2021-10-24 00:25:03 +0200

    address checks

--- web/src/verifiers/test-random-number/index.ts
+++ web/src/verifiers/test-random-number/index.ts
@@ -2,6 +2,8 @@ import { Task } from "types"
 
 export async function verify(task: Task, address: string): Promise<boolean | number>
 {
+    if (!address) return false
+    
     const nr = Math.floor((Math.random() * 10) + 1)
     return nr * task.points
 }
