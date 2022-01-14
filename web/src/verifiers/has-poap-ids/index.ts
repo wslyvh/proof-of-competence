651daf93801aa2c60c1533d7094f534d71e0bc28
wslyvh <wslyvh@users.noreply.github.com>
2022-01-14 09:11:32 +0100

    poap event ids

--- web/src/verifiers/has-poap-ids/index.ts
+++ web/src/verifiers/has-poap-ids/index.ts
@@ -6,14 +6,14 @@ export async function verify(task: Task, address: string): Promise<boolean | num
     if (!Array.isArray(task.params['ids'])) return false
 
     try { 
-        const tokenIds = task.params['ids']
+        const eventIds = task.params['ids']
         const poaps = await fetch(`https://api.poap.xyz/actions/scan/${address}`)
         const data = await poaps.json()
 
         if (!Array.isArray(data) || data.length === 0) return false
-        if (!Array.isArray(tokenIds) || tokenIds.length === 0) return false
+        if (!Array.isArray(eventIds) || eventIds.length === 0) return false
 
-        return data.map(i => i.tokenId).some(i => tokenIds.includes(i))
+        return data.map(i => i.event.id).some(i => eventIds.includes(i))
     }
     catch(e) {
         return false
