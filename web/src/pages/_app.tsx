d8c40bd1c0543c8606ec82b633eadaf9581ebfc8
wslyvh <wslyvh@users.noreply.github.com>
2022-01-14 16:27:07 +0100

    plausible

--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -4,16 +4,20 @@ import { Web3ReactProvider } from '@web3-react/core'
 import { SEO } from 'components/seo'
 import { getProvider } from 'utils/web3'
 import { Layout } from 'components/layout'
+import PlausibleProvider from 'next-plausible'
+import { DOMAIN } from 'utils/constants'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider>
-      <Web3ReactProvider getLibrary={getProvider}>
-        <SEO />
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
-      </Web3ReactProvider>
-    </ChakraProvider>
+    <PlausibleProvider domain={DOMAIN} trackOutboundLinks>
+      <ChakraProvider>
+        <Web3ReactProvider getLibrary={getProvider}>
+          <SEO />
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        </Web3ReactProvider>
+      </ChakraProvider>
+    </PlausibleProvider>
   )
 }
--- web/src/utils/constants.ts
+++ web/src/utils/constants.ts
@@ -1,8 +1,9 @@
 export const TITLE = 'Proof of Competence'
 export const DESCRIPTION = 'Proof of Competence (PoC) is an open, permissionless frameworks to create on-boarding journeys into the Web3 space.'
-export const SITE_URL = `https://www.proofofcompetence.xyz/`
+export const SITE_URL = `https://www.poc.quest/`
 export const IMAGE_OG = `${SITE_URL}images/social.jpeg`
 export const SOCIAL_HANDLE = '@proofofcompetence'
+export const DOMAIN = 'poc.quest'
 
 export const DEFAULT_COLOR_SCHEME = 'teal'
 
