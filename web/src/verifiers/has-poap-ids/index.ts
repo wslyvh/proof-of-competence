9393dadddbaea68513fc1b5cb483b3533e0516f4
wslyvh <wslyvh@users.noreply.github.com>
2022-01-14 17:41:14 +0100

    bankless ids

--- web/src/verifiers/has-poap-ids/index.ts
+++ web/src/verifiers/has-poap-ids/index.ts
@@ -13,7 +13,7 @@ export async function verify(task: Task, address: string): Promise<boolean | num
         if (!Array.isArray(data) || data.length === 0) return false
         if (!Array.isArray(eventIds) || eventIds.length === 0) return false
 
-        return data.map(i => i.event.id.toString()).some(i => eventIds.includes(i))
+        return data.map(i => i.event.id).some(i => eventIds.includes(i))
     }
     catch(e) {
         return false
