dbbb2a1e549e138846d4dc3af10fa8a877f0e192
wslyvh <wslyvh@users.noreply.github.com>
2022-01-25 14:15:39 +0100

    quests

--- src/pages/api/files.ts
+++ src/pages/api/files.ts
@@ -3,10 +3,29 @@ import { join } from 'path'
 import fs from 'fs'
 
 export default function handler(req: NextApiRequest, res: NextApiResponse<Array<string>>) {
-  const dir = join(process.cwd(), 'src', 'journeys')
-  const files = fs.readdirSync(dir, { withFileTypes: true })
-    .filter(i => i.isFile() && i.name.endsWith('.json')).map(i => i.name)
-  console.log('FILES', files)
+  console.log('Files handler..')
+
+  const current = join(__dirname)
+  console.log('Current dir', current)
+
+  const files = fs.readdirSync(current, { withFileTypes: true }).map(i => i.name)
+  fs.readdirSync(current, { withFileTypes: true }).forEach(i => {
+    console.log('  -', i.name)
+  })
+
+
+  try { 
+    const questsDir = join(__dirname, 'quests')
+    console.log('Quests dir', questsDir)
+    const quests = fs.readdirSync(current, { withFileTypes: true }).map(i => i.name)
+    fs.readdirSync(current, { withFileTypes: true }).forEach(i => {
+      console.log('  -', i.name)
+    })
+  }
+  catch (e) { 
+    console.log('Unable to fetch quests')
+    console.error(e)
+  }
 
   res.status(200).json(files)
 }
