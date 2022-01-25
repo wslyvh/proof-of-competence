698f96324600727c499a5354f24cf7bed9a81989
wslyvh <wslyvh@users.noreply.github.com>
2022-01-25 09:59:08 +0100

    description

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,5 +1,5 @@
 export const TITLE = 'Proof of Competence'
-export const DESCRIPTION = 'Proof of Competence (PoC) is an open, permissionless frameworks to create on-boarding journeys into the Web3 space.'
+export const DESCRIPTION = 'Proof of Competence (PoC) is an open, permissionless frameworks to create on-chain quests and on-boarding journeys into Web3.'
 export const SITE_URL = `https://www.poc.quest/`
 export const IMAGE_OG = `${SITE_URL}images/social.png`
 export const SOCIAL_HANDLE = '@proofofcompetence'
